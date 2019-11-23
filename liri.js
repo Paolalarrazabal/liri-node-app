0fb944522899bb796b754058182d1b766d738bc9
Paola L <paolalarrazabal@gmail.com>
2019-11-23 12:02:14 -0500

    added functionons for command line argvs and included apr url, console log requiered info in data array, added usercommand function to properly utilize the findThis function in order to use text in random.txt file for command line functions

--- liri.js
+++ liri.js
@@ -1,14 +1,21 @@
 const dotenv = require('dotenv').config();
 
-const keys = require("./keys.js")
+var keys = require("./keys.js")
 
-// const spotify = new Spotify(keys.spotify); 
+var Spotify = require('node-spotify-api');
+
+
 
 const axios = require('axios');
 
+const fs = require('fs');
 
 const moment = require('moment');
 moment().format(); 
+
+
+
+
 
 const findConcert = (artist) => {
     console.log("searching"); 
@@ -29,23 +36,105 @@ const findConcert = (artist) => {
              `; 
              
             console.log(venueData);
+           
             
-            
-        }
-
-     
+            }
         });
         
+}
+
+function findSong() {
+    const spotify = new Spotify(keys.spotify);
+
+    spotify.search({ type: 'track', query: term, limit: 1}, function(err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+     
+        // console.log(data.tracks.items); 
+
+        const spotifyData = data.tracks.items;
+
+        
+        for (var i = 0; i < 1; i++) {
+            const songData = 
+            `
+            Artist(s): ${spotifyData[i].artists[0].name}
+            Song: ${spotifyData[i].name}
+            Preview Link: ${spotifyData[i].preview_url}
+            Album: ${spotifyData[i].album.name}
+            `
+            console.log(songData);
+        }
+
+        });
+
+    
 
 
 }
 
 
+const findMovie = (movie = "Mr. Nobody") => {
+    const queryUrl = `http://www.omdbapi.com/?t=${movie}&y=&plot=short&apikey=trilogy`;  
 
-const search = process.argv[2]?process.argv[2]:undefined; 
-const term = process.argv[3]?process.argv.slice(3).join(" "):undefined; 
-search === "concert-this" ? findConcert(term) : undefined; 
+    axios.get(queryUrl).then( response => {
+     console.log(response.data);
+const OMDBData = response.data; 
+     const movieData = 
+     `
+     Title: ${OMDBData.Title}
+     Year: ${OMDBData.Year}
+     IMDB Rating: ${OMDBData.imdbRating}
+     Rotten Tomatoes: ${OMDBData.Ratings[1].Value}
+     Country: ${OMDBData.Country}
+     Language: ${OMDBData.Language}
+     Plot: ${OMDBData.Plot}
+     Actors: ${OMDBData.Actors}
+     
+     `
+     console.log(movieData);
+    }
+        
+     
+        
+    )
+}
 
+
+
+const findThis = () => {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+        if (error) {
+            return console.log(error); 
+    }
+    
+    const dataArr = data.split(",");
+
+    
+    
+
+     search = dataArr[0]; 
+     term = dataArr[1]; 
+
+     userCommand(search, term);
+   
+    });
+}; 
+
+
+
+let search = process.argv[2]?process.argv[2]:undefined; 
+let term = process.argv[3]?process.argv.slice(3).join(" "):undefined; 
+
+ const userCommand = (search, term) =>  {
+    search === "concert-this" ? findConcert(term) : undefined; 
+    search === "spotify-this-song" ? findSong(term) : undefined; 
+    search === "movie-this" ? findMovie(term) : undefined; 
+    search === "do-what-it-says" ? findThis(term) : undefined;
+ }
+ 
+ userCommand(search, term); 
 
 
 
